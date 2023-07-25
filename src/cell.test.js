c06f36ea8be5dad8a899fb6ad9e0b1fdd6638a69
greeneggsanddan <greeneggsanddan@gmail.com>
2023-07-25 15:10:43 -0700

    Test Cell methods

---
+++ src/cell.test.js
@@ -0,0 +1,7 @@
+import Gameboard from "./gameboard";
+
+test('cell is shot', () => {
+  const board = new Gameboard;
+  board.array[9][9].shoot();
+  expect(board.array[9][9].isShot).toBe(true);
+});
